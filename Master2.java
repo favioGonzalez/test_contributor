6c96b985e1bfa140b909ad78515af656ccf6efe8
Favio <hugofavio@gmail.com>
2019-04-11 19:10:08 -0700

    on

--- Master2.java
+++ Master2.java
@@ -1,6 +1,6 @@
 
 public Class Master2{
   public static void main(String[] args){
-    System.out.println("hello world, this is great");
+    System.out.println("hello world, this is great, super!");
   }
 }
